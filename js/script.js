e4c656e227e88d01050e1f212f360268e6564741
Tiffany_B <97065732+tiffy3b@users.noreply.github.com>
2022-05-03 11:06:10 -0400

    exercise 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
-    const usersRequest = await fetch ("https://randomuser.me/api/?results=5");
+const getData = async function (numUsers) {
+    const usersRequest = await fetch (`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json ();
     //console.log(data);
     const userResults = data.results;
     //console.log(userResults);
     displayUsers(userResults);
 };
+getData(1);
+
 const displayUsers = function (userResults){
     randomFolks.innerHTML = "";
     for (const user of userResults) {
@@ -22,4 +25,8 @@ const displayUsers = function (userResults){
         randomFolks.append(userDiv);
     }
     };
-getData();
+
+selectUserNumber.addEventListener('change', function(e){
+    const numUsers = e.target.value;
+    getData(numUsers);
+})
